81ba884d134f9f4bd56d045b78c360ec882f569a
karin valcanover <valcanoverkarin@outlook.it>
2021-03-23 15:27:18 +0100

    scritto i vari passaggi

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 //ma per i multipli di 3 stampi “Fizz” al posto del numero
 //e per i multipli di 5 stampi Buzz. 
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
+
+//programma che stampi i numeri da 1 a 100
+
+//stampare Fizz per i multipli di 3
+
+//stampare Buzz per i multipli di 5
+
+//stampare FizzBuzz per i multipli di 3 e 5
