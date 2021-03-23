269a347e11ae3eefae8fdb3dafe6a34156799208
karin valcanover <valcanoverkarin@outlook.it>
2021-03-23 15:38:55 +0100

    inizio

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
 
 //programma che stampi i numeri da 1 a 100
+for (var i = 0; i < 100; i++) {
+    var number = i;
 
-//stampare Fizz per i multipli di 3
+    //stampare Fizz per i multipli di 3
+    if ( number % 3 == 0 ) {
+        number = 'Fizz'
+        console.log(number)
+    }
+}
+
 
 //stampare Buzz per i multipli di 5
 
